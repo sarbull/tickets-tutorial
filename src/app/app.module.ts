de894a18d89d2222c42a0d6f3339e5ecd5171bab
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-10-21 01:29:41 +0300

    Add material.module.ts and add buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import {
-  MatToolbarModule,
-  MatButtonModule,
-  MatIconModule,
-  MatMenuModule,
-  MatCardModule
-} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -17,11 +12,8 @@ import {
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatMenuModule,
-    MatCardModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [
